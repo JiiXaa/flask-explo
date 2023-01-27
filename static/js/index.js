8e28786c595541d12938c7a69f96bb1652ad96cb
JiiXaa <anythingfx@gmail.com>
2023-01-27 20:16:17 +0000

    Changed script for adding active class to the navbar. Used location API because adding event listener on the button broke flask functionality

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,24 @@
-const navigationLinks = document.querySelectorAll('#nav-link');
-
 /**
- * Add active class to the current button (highlight it)
+ * Add active class to the current button (highlight it) when the page is loaded or refreshed
  */
 
 // !! need to figure out how to add active class to link when page is refreshed or loaded !!
-navigationLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    navigationLinks.forEach((link) => {
-      link.classList.remove('active');
-    });
-    link.classList.add('active');
-  });
-});
+// navigationLinks.forEach((link) => {
+//   link.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     navigationLinks.forEach((link) => {
+//       link.classList.remove('active');
+//     });
+//     link.classList.add('active');
+//   });
+// });
+
+const currentLocation = location.href;
+const navigationLinks = document.querySelectorAll('#nav-link');
+const navigationLength = navigationLinks.length;
+
+for (let i = 0; i < navigationLength; i++) {
+  if (navigationLinks[i].href === currentLocation) {
+    navigationLinks[i].classList.add('active');
+  }
+}
